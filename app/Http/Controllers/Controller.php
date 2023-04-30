b19fe2d3330a9c3bcc186cb5b7510d4d8bb27722
benny <benny@benny-optiplex-7060>
2023-04-30 09:25:23 +0100

    added builder

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -52,89 +52,22 @@ class Controller extends BaseController
 
     public function generatePreviewImage($filePath, $imagePath)
     {
-
-        // Load the HTML content of the file
         $htmlContent = file_get_contents($filePath);
 
-        // Create a new image resource with a width of 1200px and a height of 630px
-        $image = imagecreatetruecolor(1200, 630);
+        $image = Image::canvas(1200, 630, '#ffffff')
+            ->text($htmlContent, 20, 20, function($font) {
+                $font->size(16);
+                $font->color('#000000');
+                $font->align('left');
+                $font->valign('top');
+            });
 
-        // Set the background color of the image to white
-        $bgColor = imagecolorallocate($image, 255, 255, 255);
-        imagefill($image, 0, 0, $bgColor);
+        $image->save($imagePath, 80);
 
-        $fontColor = imagecolorallocate($image, 0, 0, 0);
-
-        $x = 0;
-        $y = 16 * 2;
-
-        $lines = explode("\n", wordwrap(strip_tags($htmlContent), 80, "\n"));
-
-        foreach ($lines as $line) {
-            imagettftext($image, 16, 0, $x, $y, $fontColor, 16, $line);
-            $y += 16 * 1.5;
-        }
-
-        // Save the image as a JPEG file with a quality of 80
-        imagejpeg($image, $imagePath, 80);
-
-        // Destroy the image resource to free up memory
-        imagedestroy($image);
-
-        // Return the image file as a response
         return response()->file($imagePath);
 
     }
 
-//    public function loadTemplates()
-//    {
-//        $storage = Storage::disk('builder');
-//
-//        $paths = $storage->directories('templates');
-//
-//        return collect($paths)->map(function($path) {
-//            $name = basename($path);
-//
-//            $updatedAt = Storage::disk('builder')->exists("$path/index.html")
-//                ? Carbon::createFromTimestamp(Storage::disk('builder')->lastModified("$path/index.html"))->toDateTimeString()
-//                : Carbon::now();
-//
-//            return [
-//                'name' => $name,
-//                'updated_at' => $updatedAt,
-////                'config' => $this->getTemplateConfig(basename($path)),
-//                'thumbnail' => $this->getTemplateImagePath($name),
-//                'thumbnail_url' => $this->getTemplateImagePath($name),
-//            ];
-//        });
-//    }
-//
-//    public function loadTemplate($name)
-//    {
-//        $paths = Storage::disk('builder')->files("templates/$name");
-//
-//        $pages = collect($paths)->filter(function($path) {
-//            return Str::contains($path, '.html');
-//        })->map(function($path) use($name) {
-//            return [
-//                'name' => basename($path, '.html'),
-//                'html' => $this->storage->get($path),
-//            ];
-//        })->values();
-//
-//        return [
-//            'name' => $name,
-////            'config' => $this->getTemplateConfig($name),
-//            'thumbnail' => $this->getTemplateImagePath($name),
-//            'pages' => $pages,
-//        ];
-//    }
-//
-//
-//    public function templateExists($name)
-//    {
-//        return Storage::disk('builder')->exists("templates/$name");
-//    }
 
     /**
      * Get template image path or default.
@@ -142,16 +75,5 @@ class Controller extends BaseController
      * @param string $name
      * @return string
      */
-//    private function getTemplateImagePath($name)
-//    {
-//        $path = "templates/$name/thumbnail.png";
-//
-//        if (Storage::disk('builder')->exists($path)) {
-//            return Storage::disk('builder')->url($path);
-//        }
-//
-//        return base_path('builder')->url(self::DEFAULT_THUMBNAIL);
-//    }
-
 
 }
